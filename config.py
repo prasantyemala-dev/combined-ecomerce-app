22457e4d2c61a6767269340afa6dc675af93c250
prasantyemala-dev <prasantyemala@gmail.com>
2025-08-30 23:05:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@
 # It's important to keep these secret.
 # In a real application, you would use environment variables
 # or a secret management system.
-AMAZON_ACCESS_KEY = "prasant2580-21"
+AMAZON_ACCESS_KEY = "YOUR_AMAZON_ACCESS_KEY""
 AMAZON_SECRET_KEY = "YOUR_AMAZON_SECRET_KEY"
-AMAZON_PARTNER_TAG = "YOUR_AMAZON_PARTNER_TAG"  # Your Amazon Associates tracking ID
+AMAZON_PARTNER_TAG = "prasant2580-21"  # Your Amazon Associates tracking ID
